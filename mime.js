54c976d7fc1d0895a72eb58454ac4e57e9f54b32
Jeff H. Parrish <jeffhparrish@gmail.com>
2016-04-07 19:43:08 -0700

    Use iconv-lite which has no native modules and is faster.

--- mime.js
+++ mime.js
@@ -1,6 +1,6 @@
 
 // see http://github.com/bnoordhuis/node-iconv for more info
-var Iconv = require("iconv").Iconv;
+var Iconv = require("iconv-lite");
 
 /* mime related functions - encoding/decoding etc*/
 /* TODO: Only UTF-8 and Latin1 are allowed with encodeQuotedPrintable */
@@ -12,25 +12,25 @@ var Iconv = require("iconv").Iconv;
  * - maxLength (Number): max length for a line, defaults to 78
  * - foldAnywhere (Boolean): can fold at any location (ie. in base64)
  * - afterSpace (Boolean): If [true] fold after the space
- * 
+ *
  * Folds a long line according to the RFC 5322
  *   <http://tools.ietf.org/html/rfc5322#section-2.1.1>
- * 
+ *
  * For example:
  *     Content-Type: multipart/alternative; boundary="----bd_n3-lunchhour1283962663300----"
  * will become
  *     Content-Type: multipart/alternative;
  *      boundary="----bd_n3-lunchhour1283962663300----"
- * 
+ *
  **/
 this.foldLine = function(str, maxLength, foldAnywhere, afterSpace){
     var line=false, curpos=0, response="", lf;
     maxLength = maxLength || 78;
-    
+
     // return original if no need to fold
     if(str.length<=maxLength)
         return str;
-    
+
     // read in <maxLength> bytes and try to fold it
     while(line = str.substr(curpos, maxLength)){
         if(!!foldAnywhere){
@@ -56,7 +56,7 @@ this.foldLine = function(str, maxLength, foldAnywhere, afterSpace){
         }
         curpos += line.length;
     }
-    
+
     // return folded string
     return response;
 }
@@ -67,7 +67,7 @@ this.foldLine = function(str, maxLength, foldAnywhere, afterSpace){
  * - str (String): String to be encoded
  * - encoding (String): Encoding Q for quoted printable or B (def.) for base64
  * - charset (String): Charset to be used
- * 
+ *
  * Encodes a string into mime encoded word format
  *   <http://en.wikipedia.org/wiki/MIME#Encoded-Word>
  *
@@ -75,20 +75,20 @@ this.foldLine = function(str, maxLength, foldAnywhere, afterSpace){
  *     See on õhin test
  * Becomes with UTF-8 and Quoted-printable encoding
  *     =?UTF-8?q?See_on_=C3=B5hin_test?=
- * 
+ *
  **/
 this.encodeMimeWord = function(str, encoding, charset){
     charset = charset || "UTF-8";
     encoding = encoding && encoding.toUpperCase() || "B";
-    
+
     if(encoding=="Q"){
         str = this.encodeQuotedPrintable(str, true, charset);
     }
-    
+
     if(encoding=="B"){
         str = this.encodeBase64(str);
     }
-    
+
     return "=?"+charset+"?"+encoding+"?"+str+"?=";
 }
 
@@ -97,9 +97,9 @@ this.encodeMimeWord = function(str, encoding, charset){
  * - str (String): String to be encoded
  * - encoding (String): Encoding Q for quoted printable or B (def.) for base64
  * - charset (String): Charset to be used, defaults to UTF-8
- * 
+ *
  * Decodes a string from mime encoded word format, see [[encodeMimeWord]]
- * 
+ *
  **/
 
 this.decodeMimeWord = function(str){
@@ -112,11 +112,11 @@ this.decodeMimeWord = function(str){
     if(encoding.toUpperCase()=="Q"){
         return this.decodeQuotedPrintable(text, true, charset);
     }
-    
+
     if(encoding.toUpperCase()=="B"){
         return this.decodeBase64(text);
     }
-    
+
     return text;
 }
 
@@ -127,18 +127,18 @@ this.decodeMimeWord = function(str){
  * - mimeWord (Boolean): Use mime-word mode (defaults to false)
  * - charset (String): Destination charset, defaults to UTF-8
  *   TODO: Currently only allowed charsets: UTF-8, LATIN1
- * 
- * Encodes a string into Quoted-printable format. 
+ *
+ * Encodes a string into Quoted-printable format.
  **/
 this.encodeQuotedPrintable = function(str, mimeWord, charset){
     charset = charset || "UTF-8";
-    
+
     /*
      * Characters from 33-126 OK (except for =; and ?_ when in mime word mode)
-     * Spaces + tabs OK (except for line beginnings and endings)  
+     * Spaces + tabs OK (except for line beginnings and endings)
      * \n + \r OK
      */
-    
+
     str = str.replace(/[^\sa-zA-Z\d]/gm,function(c){
         if(!!mimeWord){
             if(c=="?")return "=3F";
@@ -148,7 +148,7 @@ this.encodeQuotedPrintable = function(str, mimeWord, charset){
             return c;
         return c=="="?"=3D":(charset=="UTF-8"?encodeURIComponent(c):escape(c)).replace(/%/g,'=');
     });
-    
+
     str = lineEdges(str);
 
     if(!mimeWord){
@@ -176,8 +176,8 @@ this.encodeQuotedPrintable = function(str, mimeWord, charset){
  * - str (String): String to be decoded
  * - mimeWord (Boolean): Use mime-word mode (defaults to false)
  * - charset (String): Charset to be used, defaults to UTF-8
- * 
- * Decodes a string from Quoted-printable format. 
+ *
+ * Decodes a string from Quoted-printable format.
  **/
 this.decodeQuotedPrintable = function(str, mimeWord, charset){
     charset = charset && charset.toUpperCase() || "UTF-8";
@@ -205,8 +205,8 @@ this.decodeQuotedPrintable = function(str, mimeWord, charset){
  * mime.encodeBase64(str) -> String
  * - str (String): String to be encoded into Base64
  * - charset (String): Destination charset, defaults to UTF-8
- * 
- * Encodes a string into Base64 format. Base64 is mime-word safe. 
+ *
+ * Encodes a string into Base64 format. Base64 is mime-word safe.
  **/
 this.encodeBase64 = function(str, charset){
     var buffer;
@@ -221,17 +221,17 @@ this.encodeBase64 = function(str, charset){
  * mime.decodeBase64(str) -> String
  * - str (String): String to be decoded from Base64
  * - charset (String): Source charset, defaults to UTF-8
- * 
+ *
  * Decodes a string from Base64 format. Base64 is mime-word safe.
- * NB! Always returns UTF-8 
+ * NB! Always returns UTF-8
  **/
 this.decodeBase64 = function(str, charset){
     var buffer = new Buffer(str, "base64");
-    
+
     if(charset && charset.toUpperCase()!="UTF-8"){
         return fromCharset(charset, buffer);
     }
-    
+
     // defaults to utf-8
     return buffer.toString("UTF-8");
 }
@@ -239,7 +239,7 @@ this.decodeBase64 = function(str, charset){
 /**
  * mime.parseHeaders(headers) -> Array
  * - headers (String): header section of the e-mail
- * 
+ *
  * Parses header lines into an array of objects (see [[parseHeaderLine]])
  * FIXME: This should probably not be here but in "envelope" instead
  **/
@@ -261,7 +261,7 @@ this.parseHeaders = function(headers){
             header_lines[name.toLowerCase()].push(parseHeaderLine(name, value));
         }
     }
-    
+
     return header_lines;
 }
 
@@ -271,7 +271,7 @@ this.parseHeaders = function(headers){
  * parseHeaderLine(name, value) -> Object
  * - name (String): Name of the header line
  * - value (String): Value of the header line
- * 
+ *
  * Parses header line into separate parts. Fore example
  *     Content-type: text/plain; Charset = utf-8
  * becomes
@@ -293,7 +293,7 @@ function parseHeaderLine(name, value){
         value: "",
         params: []
     }, parts = value.split(/;/g), i, line, m, n, v;
-    
+
     for(i=0; i<parts.length;i++){
         line = parts[i].replace(/=\?([^\?]+)\?([^\?]+)\?([^\?]+)\?=/g, function(a){
             return exports.decodeMimeWord(a);
@@ -320,17 +320,17 @@ function parseHeaderLine(name, value){
 /**
  * lineEdges(str) -> String
  * - str (String): String to be processed
- * 
+ *
  * Replaces all spaces and tabs in the beginning and end of the string
  * with quoted printable encoded chars. Needed by [[encodeQuotedPrintable]]
  **/
 function lineEdges(str){
     str = str.replace(/^[ \t]+/gm, function(wsc){
-        return wsc.replace(/ /g,"=20").replace(/\t/g,"=09"); 
+        return wsc.replace(/ /g,"=20").replace(/\t/g,"=09");
     });
-    
+
     str = str.replace(/[ \t]+$/gm, function(wsc){
-        return wsc.replace(/ /g,"=20").replace(/\t/g,"=09"); 
+        return wsc.replace(/ /g,"=20").replace(/\t/g,"=09");
     });
     return str;
 }
@@ -340,39 +340,40 @@ function lineEdges(str){
  * - charset (String): Source charset
  * - buffer (Buffer): Buffer in <charset>
  * - keep_buffer (Boolean): If true, return buffer, otherwise UTF-8 string
- * 
+ *
  * Converts a buffer in <charset> codepage into UTF-8 string
  **/
-function fromCharset(charset, buffer, keep_buffer){
-    var iconv = new Iconv(charset,'UTF-8'),
-        buffer = iconv.convert(buffer);
-    return keep_buffer?buffer:buffer.toString("utf-8");
+function fromCharset(charset, buffer, keep_buffer) {
+    var sourceString = Iconv.decode(buffer, charset);
+    var utf8Buffer = Iconv.encode(sourceString, 'utf-8');
+    return keep_buffer ? utf8Buffer : buffer.toString('utf-8');
 }
 
 /**
  * toCharset(charset, buffer) -> Buffer
  * - charset (String): Source charset
  * - buffer (Buffer): Buffer in UTF-8 or string
- * 
+ *
  * Converts a string or buffer to <charset> codepage
  **/
-function toCharset(charset, buffer){
-    var iconv = new Iconv('UTF-8',charset);
-    return iconv.convert(buffer);
+function toCharset(charset, buffer) {
+    var charsetString = Iconv.decode(buffer, charset);
+    var charsetBuffer = Iconv.encode(charsetString, charset);
+    return charsetBuffer;
 }
 
 /**
  * decodeBytestreamUrlencoding(encoded_string) -> Buffer
  * - encoded_string (String): String in urlencode coding
- * 
+ *
  * Converts an urlencoded string into a bytestream buffer. If the used
  * charset is known the resulting string can be converted to UTF-8 with
- * [[fromCharset]]. 
- * NB! For UTF-8 use decodeURIComponent and for Latin 1 decodeURL instead 
+ * [[fromCharset]].
+ * NB! For UTF-8 use decodeURIComponent and for Latin 1 decodeURL instead
  **/
 function decodeBytestreamUrlencoding(encoded_string){
 
-    var c, i, j=0, buffer_length = encoded_string.length - 
+    var c, i, j=0, buffer_length = encoded_string.length -
                             (encoded_string.match(/%/g).length*2),
         buffer = new Buffer(buffer_length);
 
